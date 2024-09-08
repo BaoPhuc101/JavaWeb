c7e034431f1ecd1bc9bc66f0b9437b41b0123b9a
BaoPhuc101 <22110200@student.hcmute.edu.vn>
2024-09-08 17:22:39 +0700

    WEEK 1

---
+++ HelloWorld/src/main/java/vn/iotstar/controllers/HomeController.java
@@ -0,0 +1,33 @@
+package vn.iotstar.controllers;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = { "/trangchu" })
+public class HomeController extends HttpServlet {
+	private static final long serialVersionUID = 1311042L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		resp.setCharacterEncoding("UTF-8");
+		req.setCharacterEncoding("UTF-8");
+
+		String ten = req.getParameter("ten");
+		String ho = req.getParameter("ho");
+
+		PrintWriter printW = resp.getWriter();
+		printW.println(ho + " " + ten);
+		printW.close();
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doGet(req, resp);
+	}
+}
